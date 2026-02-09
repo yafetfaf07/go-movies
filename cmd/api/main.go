4c434a085d5f30fa93951aeab42a36f834214e22
Yafet Yosef <japhetkumalo23@gmail.com>
2026-02-09 18:49:13 +0300

    Add database configuration and connection handling; implement migration files for movies table and constraints

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,70 +1,102 @@
 package main
+
 import (
-"flag"
-"fmt"
-"log/slog"
-"net/http"
-"os"
-"time"
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
-// Declare a string containing the application version number. Later in the book we'll
-// generate this automatically at build time, but for now we'll just store the version
-// number as a hard-coded global constant.
+
 const version = "1.0.0"
-// Define a config struct to hold all the configuration settings for our application.
-// For now, the only configuration settings will be the network port that we want the
-// server to listen on, and the name of the current operating environment for the
-// application (development, staging, production, etc.). We will read in these
-// configuration settings from command-line flags when the application starts.
+
 type config struct {
-port int
-env string
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
-// Define an application struct to hold the dependencies for our HTTP handlers, helpers,
-// and middleware. At the moment this only contains a copy of the config struct and a
-// logger, but it will grow to include a lot more as our build progresses.
+
 type application struct {
-config config
-logger *slog.Logger
+	config config
+	logger *slog.Logger
 }
 
 func main() {
-	// Declare an instance of the config struct.
 	var cfg config
-	// Read the value of the port and env command-line flags into the config struct. We
-	// default to using the port number 4000 and the environment "development" if no
-	// corresponding flags are provided.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pass123@localhost/greenlight", "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 	flag.Parse()
-	// Initialize a new structured logger which writes log entries to the standard out
-	// stream.
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	// Declare an instance of the application struct, containing the config struct and
 	// the logger.
-	app := &application{
-	config: cfg,
-	logger: logger,
-	}
-	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
-	// to the healthcheckHandler method (which we will create in a moment).
 
-	// Declare a HTTP server which listens on the port provided in the config struct,
-	// uses the servemux we created above as the handler, has some sensible timeout
-	// settings and writes any log messages to the structured logger at Error level.
+	db, err := openDB(cfg)
+
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
 	srv := &http.Server{
-		Addr:
-		fmt.Sprintf(":%d", cfg.port),
-		Handler:app.routes(),
-		IdleTimeout:time.Minute,
-		ReadTimeout:5*time.Second,		
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		ErrorLog:
-		slog.NewLogLogger(logger.Handler(), slog.LevelError),
-		}
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
 	// Start the HTTP server.
 	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
 	}
+
+
+db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yafetfaf07/go-movies
 
 go 1.22.2
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.11.1 // indirect
+)
